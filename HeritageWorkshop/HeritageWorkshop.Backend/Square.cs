24a492ce2cc439f88de3287dc3483ed5d06afa12
Jorge <Jorge@DESKTOP-1B7HDOA>
2026-02-22 14:20:58 -0500

    Add square

---
+++ HeritageWorkshop/HeritageWorkshop.Backend/Square.cs
@@ -0,0 +1,32 @@
+﻿namespace HeritageWorkshop.Backend;
+
+public class Square : GeometricFigure
+{
+    // Fields
+    private double _a;
+
+    // Constructors
+    public Square(string name, double a) : base(name)
+    {
+        A = a;
+    }
+
+    // Properties
+    public double A
+    {
+        get => _a;
+        set => _a = ValidateA(value);
+    }
+
+    // Methods
+    public override double GetArea() => A * A;
+
+    public override double GetPerimeter() => 4 * A;
+
+    private double ValidateA(double value)
+    {
+        if (value <= 0)
+            throw new ArgumentOutOfRangeException(nameof(A), "A must be greater than 0");
+        return value;
+    }
+}
