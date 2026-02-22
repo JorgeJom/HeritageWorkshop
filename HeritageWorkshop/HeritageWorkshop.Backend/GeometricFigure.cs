f374fb651ef48fb1a4a82946fab55eb5c73b8a39
Jorge <Jorge@DESKTOP-1B7HDOA>
2026-02-22 14:12:34 -0500

    Add class diagram

--- HeritageWorkshop/HeritageWorkshop.Backend/GeometricFigure.cs
+++ HeritageWorkshop/HeritageWorkshop.Backend/GeometricFigure.cs
@@ -2,5 +2,23 @@
 
 public abstract class GeometricFigure
 {
+    // Properties
+    public string Name
+    { 
+        get; 
+        set;
+    }
 
+    // Constructors
+    protected GeometricFigure(string name)
+    {
+        Name = name;
+    }
+
+    // Methods
+    public abstract double GetArea();
+    public abstract double GetPerimeter();
+
+    public override string ToString() =>
+        $"{Name,-15} => Area......: {GetArea(),10:N5}    Perimeter: {GetPerimeter(),10:N5}";
 }
