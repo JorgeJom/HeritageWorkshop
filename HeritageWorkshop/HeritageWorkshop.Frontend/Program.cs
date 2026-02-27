d0e5aeee70c1fdb93b1c3aeb19b6b8a978f7ecc2
Jorge <Jorge@DESKTOP-1B7HDOA>
2026-02-26 19:14:52 -0500

    Validate program

--- HeritageWorkshop/HeritageWorkshop.Frontend/Program.cs
+++ HeritageWorkshop/HeritageWorkshop.Frontend/Program.cs
@@ -7,7 +7,7 @@ var kite = new Kite(nameof(Kite), 7, 8, 5, 6);
 var rectangle = new Rectangle(nameof(Rectangle), 4.568, 67.790);
 var parallelogram = new Parallelogram(nameof(Parallelogram), 14.65, 54.67, 23.09);
 var triangle = new Triangle(nameof(Triangle), 45.56, 12.34, 27.09, 15);
-var trapeze = new Trapeze(nameof(Trapeze), 10, 20, 30, 40, 200);
+var trapeze = new Trapeze(nameof(Trapeze), 10, 20, 30, 40, 20);
 
 var figures = new List<GeometricFigure>() { circle, square, rhombus, kite, rectangle, parallelogram, triangle, trapeze };
 
